7f16e4e8b3f6915cf54c0f98f5fcc200ec289c82
Sergey <lebedev_81@magora-systems.com>
2018-06-20 17:59:16 +0300

    Add API request

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {
     StyleSheet,
+    View,
+    ImageBackground,
     Text,
     KeyboardAvoidingView,
     Platform,
-    View,
-    ImageBackground,
+    ActivityIndicator,
+    StatusBar,
 } from 'react-native';
 
+import {fetchLocationId, fetchWeather} from "./utils/api";
 import getImageForWeather from './utils/getImageForWeather';
 
 import SearchInput from './components/SearchInput';
@@ -16,35 +19,79 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            location: 'San Francisco',
+            loading: false,
+            error: false,
+            location: '',
+            temperature: 0,
+            weather: '',
         }
     }
 
+    componentDidMount() {
+        this.handleUpdateLocation('San Francisco');
+    }
+
     handleUpdateLocation = city => {
-        this.setState({
-            location: city,
-        })
+        if (!city) return;
+
+        this.setState({loading: true}, async () => {
+            try {
+                const locationId = await fetchLocationId(city);
+                const {location, weather, temperature} = await fetchWeather(locationId);
+                this.setState({
+                    loading: false,
+                    error: false,
+                    location,
+                    weather,
+                    temperature,
+                });
+            } catch (e) {
+                this.setState({
+                    loading: false,
+                    error: true,
+                });
+            }
+        });
     };
 
     render() {
-        const {location} = this.state;
+        const {loading, error, location, weather, temperature} = this.state;
         return (
             <KeyboardAvoidingView style={styles.container} behavior="padding">
+                <StatusBar barStyle="light-content"/>
                 <ImageBackground
-                    source={getImageForWeather('Clear')}
+                    source={getImageForWeather(weather)}
                     style={styles.imageContainer}
                     imageStyle={styles.image}
                 >
-
                     <View style={styles.detailsContainer}>
-                        <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
-                        <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
-                        <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
-
-                        <SearchInput
-                            placeholder="Search any city"
-                            onSubmit={this.handleUpdateLocation}
-                        />
+                        <ActivityIndicator animating={loading} color="white" size="large"/>
+                        {!loading && (
+                            <View>
+                                {error && (
+                                    <Text style={[styles.smallText, styles.textStyle]}>
+                                        Cloud not weather, please try a different city.
+                                    </Text>
+                                )}
+                                {!error && (
+                                    <View>
+                                        <Text style={[styles.largeText, styles.textStyle]}>
+                                            {location}
+                                        </Text>
+                                        <Text style={[styles.smallText, styles.textStyle]}>
+                                            {weather}
+                                        </Text>
+                                        <Text style={[styles.largeText, styles.textStyle]}>
+                                            {`${Math.round(temperature)}˚`}
+                                        </Text>
+                                    </View>
+                                )}
+                                <SearchInput
+                                    placeholder="Search any city"
+                                    onSubmit={this.handleUpdateLocation}
+                                />
+                            </View>
+                        )}
                     </View>
                 </ImageBackground>
             </KeyboardAvoidingView>
